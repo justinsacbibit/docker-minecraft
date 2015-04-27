d8dfb068b1b18ff8ec90414255f59de70a1f3014
Justin Sacbibit <jsacbibit@gmail.com>
2015-04-26 21:56:38 -0400

    Copy start.sh to /minecraft directory for restart

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ if [ ! -f /minecraft/spigot.jar ]; then
     cp /spigot.jar /minecraft/spigot.jar
 fi
 
+if [ ! -f /minecraft/start.sh ]; then
+    cp /start.sh /minecraft/start.sh
+fi
+
 # Accept EULA
 echo eula=true > /minecraft/eula.txt
 
