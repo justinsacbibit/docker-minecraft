6ee7ed057dc0aa20f444f437b1bde5397f737620
Justin Sacbibit <jsacbibit@gmail.com>
2015-04-27 00:20:18 -0400

    Re-enable LWC

--- plugins.sh
+++ plugins.sh
@@ -24,9 +24,10 @@ wget -q http://dev.bukkit.org/media/files/866/562/AdminEssentials.jar -O /minecr
 # PermissionsEx
 wget -q http://dev.bukkit.org/media/files/874/950/PermissionsEx-1.23.2.jar -O /minecraft/plugins/permissionsex.jar
 # LWC
-# wget -q http://dev.bukkit.org/media/files/718/126/LWC.jar -O /minecraft/plugins/lwc.jar
+wget -q http://dev.bukkit.org/media/files/718/126/LWC.jar -O /minecraft/plugins/lwc.jar
 
 # Root permissions should be empty
+echo Writing empty root permissions.yml
 echo > /minecraft/permissions.yml
 # Copy permissions if it doesn't exist
 if [ ! -f /minecraft/plugins/PermissionsEx/permissions.yml ]; then
